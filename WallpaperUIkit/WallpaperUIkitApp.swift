2ff8908b1372fe9f8462dc8271cb56eebd456bb6
gunnargray <hi@gunnargray.com>
2025-06-26 12:45:17 -0500

    Initial Commit

---
+++ WallpaperUIkit/WallpaperUIkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperUIkitApp.swift
+//  WallpaperUIkit
+//
+//  Created by Gunnar Gray on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperUIkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
